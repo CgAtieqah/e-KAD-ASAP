ecceee2b8d5e78525d4ca2bd14453e8e5ef0e1c7
CgAtieqah <cikguatieqah@gmail.com>
2026-08-19 21:48:29 +0800

    Create new repository

---
+++ lessons.js
@@ -0,0 +1,109 @@
+const lessons = [
+  {
+    id: 'babat',
+    part1: 'BA',
+    part2: 'BAT',
+    audio: 'audio/babat.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'babas',
+    part1: 'BA',
+    part2: 'BAS',
+    audio: 'audio/babas.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'bobom',
+    part1: 'BO',
+    part2: 'BOM',
+    audio: 'audio/bobom.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'cacat',
+    part1: 'CA',
+    part2: 'CAT',
+    audio: 'audio/cacat.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'zizip',
+    part1: 'ZI',
+    part2: 'ZIP',
+    audio: 'audio/zizip.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'lalam',
+    part1: 'LA',
+    part2: 'LAM',
+    audio: 'audio/lalam.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'babal',
+    part1: 'BA',
+    part2: 'BAL',
+    audio: 'audio/babal.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'lulur',
+    part1: 'LU',
+    part2: 'LUR',
+    audio: 'audio/lulur.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'mamak',
+    part1: 'MA',
+    part2: 'MAK',
+    audio: 'audio/mamak.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'sasak',
+    part1: 'SA',
+    part2: 'SAK',
+    audio: 'audio/sasak.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'yayam',
+    part1: 'YA',
+    part2: 'YAM',
+    audio: 'audio/yayam.mp3',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'sasam',
+    part1: 'SA',
+    part2: 'SAM',
+    audio: 'audio/sasam.m4a',
+    highlight1: 200,
+    highlight2: 1700
+  },
+  {
+    id: 'bubuh',
+    part1: 'BU',
+    part2: 'BUH',
+    audio: 'audio/bubuh.m4a',
+    highlight1: 200,
+    highlight2: 1700
+  },
+
+  
+];
+
